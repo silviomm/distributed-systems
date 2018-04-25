1920d56c300724360d634d6836633dc790d65dd0
silviomm <silviommattos@gmail.com>
2018-04-25 15:06:22 -0300

    tratando retorno do servidor

--- trab_1/app/cliente.c
+++ trab_1/app/cliente.c
@@ -4,9 +4,24 @@ void ve_quem_ta_online(char* ip) {
   printf("Pegando lista de usuários...\n");
   TSocket sock = ConnectToServer(ip, 2018); //2018 porta padrão do projeto
   
-  char str[5] = "2\n";
+  /* envia comando de listagem */
+  char str[100] = "2\n";
   if (WriteN(sock, str, strlen(str)) <= 0) { 
     ExitWithError("WriteN() desconecta_servidor_usuarios failed"); 
+  }
+
+  /* recebe quantidade e lista de usuarios*/
+  if (ReadLine(sock, str, 99) < 0)
+    ExitWithError("ReadLine() failed");
+  else 
+    printf("\nExistem %s usuarios online...\n",str);
+  int qnt;
+  sscanf(str, "%d", &num);
+  for (int i=0; i < num; i++) {
+    if (ReadLine(sock, str, 99) < 0)
+      ExitWithError("ReadLine() failed");
+    else 
+      printf("%s",str);    
   }
 }
 
